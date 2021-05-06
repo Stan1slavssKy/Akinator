2528b832da6888852d123f0ed310140a0f6df2aa
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2021-05-06 07:44:24 +0300

    Add files via upload

---
+++ akinator/akinator.h
@@ -0,0 +1,14 @@
+#ifndef AKINATOR_INCLUDED
+#define AKINATOR_INCLUDED
+
+//===================================================================================
+
+struct akinator
+{
+    tree_node* root = nullptr;
+    char* answer    = nullptr;
+};
+
+//===================================================================================
+
+#endif
---
+++ akinator/main.cpp
@@ -0,0 +1,12 @@
+#include "akinator.h"
+#include "tree.h"
+
+int main ()
+{
+    tree_node tree = {};  
+    akinator  aktr = {};
+    
+    tree_construct (&tree);
+
+    return 0;
+}
