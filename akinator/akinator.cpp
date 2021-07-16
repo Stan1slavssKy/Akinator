c4aa19e1dd464c74681a6c9177191789acb42ac8
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2021-07-16 20:09:38 +0300

    Add files via upload

--- akinator/akinator.cpp
+++ akinator/akinator.cpp
@@ -7,7 +7,7 @@ void akinator_construct (akinator_tree* aktr, char* file_name)
     assert (aktr);
     assert (file_name);
 
-    aktr -> base_info = (text*) calloc (1, sizeof(text*));
+    aktr -> base_info = (text*) calloc (1, sizeof (text*));
     assert (aktr -> base_info);
 
     input_inform (file_name, aktr -> base_info);
@@ -27,12 +27,9 @@ void akinator_menu (akinator_tree* aktr)
     assert (aktr);
     assert (aktr -> root);
 
-    printf ("Hi, I'm Akinator, please choise game mode:\n"
-    "1) guessing game\n"
-    "2) check base\n"
-    "3) exit\n");
+    print_hello ();
 
-    aktr -> game_mode = processing_mod_input ();
+    aktr -> game_mode = handling_mod_input ();
 
     int game_mode = aktr -> game_mode;
 
@@ -42,6 +39,8 @@ void akinator_menu (akinator_tree* aktr)
         {
             printf ("You choice guessing game, lets start!\n");
             akinator_mode_1 (aktr, aktr -> root);
+            
+            akinator_menu   (aktr);
             break;
         }
             
@@ -50,6 +49,8 @@ void akinator_menu (akinator_tree* aktr)
             printf ("You have chosen to view the database. Here it is:\n");
             const char string[] = "firefox GraphViz/base_dump.png";
             system (string);
+            
+            akinator_menu (aktr);
             break;
         }
 
@@ -69,7 +70,17 @@ void akinator_menu (akinator_tree* aktr)
 
 //===================================================================================
 
-int processing_mod_input ()
+void print_hello ()
+{
+    printf ("Hi, I'm Akinator, please choise game mode:\n"
+    "1) guessing game\n"
+    "2) check base\n"
+    "3) exit\n");
+}
+
+//===================================================================================
+
+int handling_mod_input ()
 {
     char* mode_buff = (char*) calloc (MAX_SYM, sizeof (char));;
     assert (mode_buff);
@@ -111,21 +122,26 @@ void akinator_mode_1 (akinator_tree* aktr, tree_node* cur_node)
     while (cur_node != nullptr)
     {
         printf ("%s\n", cur_node -> data);
+
         cur_node = get_answer (aktr, cur_node);
 
         if (cur_node -> left == nullptr && cur_node -> right == nullptr)
         {
             printf ("I think it is %s.\n", cur_node -> data);
+                    
+            tree_node* prev_cur_node = cur_node;
+                    
             cur_node = get_answer (aktr, cur_node);
     
-            if (cur_node == nullptr && aktr -> answer == 1)
+            if (cur_node == nullptr && aktr -> answer == YES)
             {
                 printf ("Thanks for game. Good bye!\n");
                 return;
             }
-            else if (cur_node == nullptr && aktr -> answer == 0)
+            else if (cur_node == nullptr && aktr -> answer == NO)
             {
-                printf ("I don't know what are you thinkong about...\n");
+                printf ("I don't know what are you thinking about...\n");
+                akinator_training (aktr, prev_cur_node);
                 return;
             }
         }
@@ -138,14 +154,14 @@ tree_node* get_answer (akinator_tree* aktr, tree_node* cur_node)
 {
     assert (cur_node);
 
-    int answ = processing_answer_input ();
+    int answ = handling_answer_input ();
 
-    if (answ == 0)
+    if (answ == NO)
     {
         aktr -> answer = answ;
         cur_node = cur_node -> left;
     }
-    else if (answ == 1)
+    else if (answ == YES)
     {
         aktr -> answer = answ;
         cur_node = cur_node -> right;
@@ -161,7 +177,7 @@ tree_node* get_answer (akinator_tree* aktr, tree_node* cur_node)
 
 //===================================================================================
 
-int processing_answer_input ()
+int handling_answer_input ()
 {
     char* answer_buff = (char*) calloc (MAX_SYM, sizeof (char));
     assert (answer_buff);
@@ -171,7 +187,7 @@ int processing_answer_input ()
     while (ans_nmb == -1)
     {
         fgets (answer_buff, MAX_SYM, stdin);
-        char* pointer = strchr(answer_buff, '\n');
+        char* pointer = strchr (answer_buff, '\n');
         *pointer = '\0';
 
         if (pointer - answer_buff > 3)
@@ -201,6 +217,58 @@ int processing_answer_input ()
 
 //===================================================================================
 
+void akinator_training (akinator_tree* aktr, tree_node* cur_node)
+{
+    assert (aktr);
+    assert (cur_node);
+
+    cur_node -> right = (tree_node*) calloc (1, sizeof (tree_node));
+    assert (cur_node -> right);
+
+    cur_node -> left = (tree_node*) calloc (1, sizeof (tree_node));
+    assert (cur_node -> left);
+
+    printf ("Please write a question that shows the difference between your object and %s?\n", cur_node -> data);
+    handling_training_input (cur_node);
+
+    printf ("Please enter your word:\n");
+    handling_training_input (cur_node -> right);
+
+    printf ("Please enter word which you will think about when you give a negative answer to your question:\n");
+    handling_training_input (cur_node -> left);
+    
+    akinator_graph (aktr);
+}
+
+//===================================================================================
+
+void handling_training_input (tree_node* cur_node)
+{   
+    char* answ = (char*) calloc (MAX_SYM, sizeof (char));
+    assert (answ);
+
+    fgets (answ, MAX_SYM, stdin);
+
+    char* pointer = strchr (answ, '\n');
+    *pointer = '\0';
+
+    int len = pointer - answ;
+    
+    if (cur_node -> data == nullptr)
+    {
+        cur_node -> data = (char*) calloc (len, sizeof (char));
+        assert (cur_node -> data);
+    }
+
+    strcpy (cur_node -> data, answ);
+
+    printf ("\t\t\t[%s]\n", cur_node -> data);
+
+    free (answ);
+}
+
+//===================================================================================
+
 void akinator_destruct (akinator_tree* aktr)
 {
     assert (aktr);
@@ -208,7 +276,7 @@ void akinator_destruct (akinator_tree* aktr)
     assert (aktr -> base_info);
     assert ((aktr -> base_info) -> file_buffer);
 
-    tree_destuct (aktr -> root);
+    tree_destruct (aktr -> root);
     free ((aktr -> base_info) -> file_buffer);
     free (aktr -> base_info);
 }
--- akinator/akinator.h
+++ akinator/akinator.h
@@ -3,18 +3,33 @@
 
 #include "../tree/tree.h"
 
-#define MAX_MODE_SYM 10
-#define CODE_ZERO 48
+//===================================================================================
+
+const int MAX_SYM   = 100;
+const int CODE_ZERO = 48;
+
+//===================================================================================
+
+enum answer
+{
+    NO  = 0,
+    YES = 1
+};
 
 //===================================================================================
 
 void  akinator_construct   (akinator_tree* aktr, char* file_name);
 void  akinator_destruct    (akinator_tree* aktr);
 void  akinator_menu        (akinator_tree* aktr);
+void  print_hello          ();
+void  akinator_training    (akinator_tree* aktr, tree_node* cur_node);
 void  akinator_mode_1      (akinator_tree* aktr, tree_node* cur_node);
-int   processing_mod_input ();
 
-tree_node* get_answer           (akinator_tree* aktr, tree_node* cur_node);
+int   handling_mod_input      ();
+int   handling_answer_input   ();
+void  handling_training_input (tree_node* cur_node);
+
+tree_node* get_answer (akinator_tree* aktr, tree_node* cur_node);
 
 //===================================================================================
 
